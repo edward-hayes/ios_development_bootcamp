c4251381eebd502b66248047026c019ff28ab0e7
= <edward.hayes@lightspeedhq.com>
2023-06-15 16:49:54 -0400

    Add section 3
    Add section 3 which is a test. Same as section 2 but DIY

---
+++ section_3/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Edward Hayes on 6/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
